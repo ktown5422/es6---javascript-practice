4680f0c6002c86f3b414e6fb1e01a9376244ef7b
Kevin Townson <ktown5422@gmail.com>
2019-11-11 21:53:56 +0000

    add .find method

--- main.js
+++ main.js
@@ -57,3 +57,75 @@ var userData = function(data) {
 userData(userInfo)
 */
 
+// Destructuring Arrays
+/*
+var cars = ['honda', 'bmw', 'benz']
+// es5
+var kevinCar = cars[0]
+var coreyCar = cars[1]
+var mikeCar = cars[2]
+
+// es6 way
+var [kevinCar, coreyCar, mikeCar] = cars;
+
+console.log(kevinCar, coreyCar, mikeCar);
+*/
+
+// forEach Method
+/*
+var users = ["Kevin", "Matt", "Joe", "James"]
+
+for (var i = 0; i < users.length; i++) {
+  console.log(users[i])
+}
+// es6 way
+users.forEach((user) => {
+  console.log(user)
+})
+*/
+
+// Map Method
+/*
+var users = [
+  {name: "Kevin", age: 27},
+  {name: "James", age: 21},
+  {name: "polo", age: 29},
+]
+
+var userNames = users.map((user) => {
+  return user.name
+})
+console.log(userNames)
+*/
+
+/*
+// Filter 
+
+var users = [
+  {name: "Kevin", age: 27},
+  {name: "James", age: 21},
+  {name: "polo", age: 29},
+]
+
+var newData = users.filter((user) => {
+  var name = user.name.toLowerCase()
+  name = name.startsWith('k')
+  return user.age > 21
+})
+
+console.log(newData)
+*/
+
+// Find 
+/*
+var users = [
+  {name: "Kevin", age: 27},
+  {name: "James", age: 21},
+  {name: "polo", age: 29},
+]
+
+var newData = users.find((user) => {
+  return user.name == "Kevin" && user.age == 27
+})
+console.log(newData)
+*/
